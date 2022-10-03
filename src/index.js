85d263203f6f736e536bf7c241f332fb2f8bce34
Harris Schneiderman <schne324@gmail.com>
2022-10-03 09:19:22 -0700

    react@16 (react 18 no worky with cauldron)

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react'
-import { createRoot } from 'react-dom/client'
+import { render } from 'react-dom'
 import App from './containers/App'
 import './index.css'
 import '@deque/cauldron-styles/dist/index.css'
 
 const container = document.getElementById('root')
-const root = createRoot(container)
-root.render(<App />)
+
+render(<App />, container)
